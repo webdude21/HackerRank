50b8f41e3805a8640913c18d799a761088ef3417
webdude21 <dimo.petrof@gmail.com>
2015-12-13 19:18:56 +0200

    Solution works fine without memoization optimization

--- src/com/webdude/algorithms/ExtraLongFactorials.java
+++ src/com/webdude/algorithms/ExtraLongFactorials.java
@@ -1,32 +1,22 @@
 package com.webdude.algorithms;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class ExtraLongFactorials {
-
-    private static Map<BigInteger, BigInteger> calculated = new HashMap<>();
 
     private static BigInteger one = BigInteger.valueOf(1);
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        BigInteger n = in.nextBigInteger();
-
-        System.out.println(calcFactorial(n));
+        System.out.println(calcFactorial(in.nextBigInteger()));
     }
 
-    public static BigInteger calcFactorial(BigInteger input){
-        if (input.equals(one)){
+    public static BigInteger calcFactorial(BigInteger input) {
+        if (input.equals(one)) {
             return one;
         }
 
-        if (!calculated.containsKey(input)){
-            calculated.put(input, input.multiply(calcFactorial(input.subtract(one))));
-        }
-
-        return calculated.get(input);
+        return input.multiply(calcFactorial(input.subtract(one)));
     }
 }
