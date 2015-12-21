572631b979957d4c0d1757ecbe81c1203401338b
webdude21 <dimo.petrof@gmail.com>
2015-12-21 09:52:51 +0200

    added two more solutions in hackerrank

---
+++ src/com/webdude/algorithms/multiplesof3and5/Solution.java
@@ -0,0 +1,27 @@
+package com.webdude.algorithms.multiplesof3and5;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    private static StringBuilder stringBuilder = new StringBuilder();
+
+    public static long sumOfMultiples(int i, int j, int limit) {
+        long s = --limit / i, t = limit / j, u = limit / (i * j);
+        return (i * (s * (s + 1) / 2)) + (j * (t * (t + 1) / 2)) - ((i * j) * (u * (u + 1) / 2));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int linesOfInput = scanner.nextInt();
+        String lineSeparator = System.lineSeparator();
+
+        for (int i = 0; i < linesOfInput; i++) {
+            scanner.nextLine();
+            stringBuilder.append(sumOfMultiples(3, 5, scanner.nextInt()));
+            stringBuilder.append(lineSeparator);
+        }
+
+        System.out.print(stringBuilder.toString());
+    }
+}
