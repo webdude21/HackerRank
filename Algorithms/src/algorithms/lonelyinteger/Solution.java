8bd217f409affb3761ae3a74562a836bbf0fd3e4
webdude21 <dimo.petrof@gmail.com>
2015-12-26 17:55:04 +0200

    solved lonely integer

---
+++ Algorithms/src/algorithms/lonelyinteger/Solution.java
@@ -0,0 +1,21 @@
+package algorithms.lonelyinteger;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+        Integer result = 0;
+
+        for (int i = 0; i < testCases; i++) {
+            result ^= scanner.nextInt();
+        }
+
+        System.out.println(result);
+    }
+
+}
