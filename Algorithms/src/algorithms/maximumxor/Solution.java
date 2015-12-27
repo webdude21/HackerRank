662adcd86cd985f27cc237fd0da33e8cd8dd6a76
webdude21 <dimo.petrof@gmail.com>
2015-12-27 12:23:48 +0200

    added max Xor solution

---
+++ Algorithms/src/algorithms/maximumxor/Solution.java
@@ -0,0 +1,27 @@
+package algorithms.maximumxor;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        int L = scanner.nextInt();
+        int R = scanner.nextInt();
+        int maxXor = Integer.MIN_VALUE;
+
+        for (int i = L; i <= R; i++) {
+            for (int j = L; j <= R; j++) {
+                int currentValue = i^j;
+                if (currentValue > maxXor){
+                    maxXor = currentValue;
+                }
+            }
+        }
+        
+        System.out.println(maxXor);
+    }
+
+}
