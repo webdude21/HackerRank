5a8ab913716a4cdd6cc76b60e92afd580b9de538
webdude21 <dimo.petrof@gmail.com>
2015-12-29 19:15:16 +0200

    switched with boolean array since i don't really need to know how many characters are used

--- Algorithms/src/algorithms/gameofthrones/Solution.java
+++ Algorithms/src/algorithms/gameofthrones/Solution.java
@@ -8,20 +8,21 @@ public class Solution {
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        int[] charactersOccurance = new int[30];
+        boolean[] charOddCount = new boolean[26];
         char[] input = scanner.nextLine().toCharArray();
 
         for (int i = 0; i < input.length; i++) {
-            charactersOccurance[input[i] - 'a']++;
+            charOddCount[input[i] - 'a'] = !charOddCount[input[i] - 'a'];
         }
 
-        int odd = 0;
-        for (int i = 0; i < charactersOccurance.length; i++) {
-            if (charactersOccurance[i] % 2 != 0) {
-                odd++;
+        int oddCountChars = 0;
+        
+        for (boolean isOdd : charOddCount) {
+            if (isOdd){
+                oddCountChars++;
             }
         }
 
-        System.out.println(odd < 2 ? "YES" : "NO");
+        System.out.println(oddCountChars < 2 ? "YES" : "NO");
     }
 }
