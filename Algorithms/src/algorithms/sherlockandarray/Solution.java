da6ce2b751592e59fe65086f9294f7bee755135f
webdude21 <dimo.petrof@gmail.com>
2015-12-29 15:58:26 +0200

    a tricky one to solve within the time limit

--- Algorithms/src/algorithms/sherlockandarray/Solution.java
+++ Algorithms/src/algorithms/sherlockandarray/Solution.java
@@ -4,33 +4,42 @@ import java.util.Scanner;
 
 public class Solution {
 
-	private static Scanner scanner;
+    private static Scanner scanner;
 
-	public static void main(String[] args) {
-		scanner = new Scanner (System.in);
-		int testCases = scanner.nextInt();
-		
-		for (int i = 0; i < testCases; i++) {
-			System.out.println(passesTest(scanner.nextInt()));
-		}
-	}
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
 
-	private static boolean passesTest(int nextInt) {
-		int[] testArray = new int[nextInt];
-		
-		for (int i = 0; i < testArray.length; i++) {
-			testArray[i] = scanner.nextInt();
-		}
-		
-		for (int i = 0; i < testArray.length; i++) {
-			int leftDigit = 0;
-			int rightDigit = 0;
-			if (i == 0){
-				
-			}
-		}
-		
-		return true;
-	}
+        for (int i = 0; i < testCases; i++) {
+            System.out.println(passesTest(scanner.nextInt()) ? "YES" : "NO");
+        }
+    }
 
+    private static boolean passesTest(int arrayLength) {        
+        int[] testArray = new int[arrayLength];
+
+        int rightNum = 0;
+        int leftNum = 0;
+        int currentNum = 0;
+
+        for (int i = 0; i < testArray.length; i++) {
+            currentNum = scanner.nextInt();
+            rightNum += currentNum;
+            testArray[i] = currentNum;
+        }
+
+        for (int i = 0; i < testArray.length; i++) {
+            if (i > 0) {
+                leftNum += testArray[i - 1];
+            }
+
+            rightNum -= testArray[i];
+
+            if (rightNum == leftNum) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
