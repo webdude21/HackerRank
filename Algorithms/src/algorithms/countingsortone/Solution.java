14ce838fc9ba1ba43e775c278bef6bea0cbf94fb
webdude21 <dimo.petrof@gmail.com>
2016-02-23 21:53:42 +0200

    added counting sort

---
+++ Algorithms/src/algorithms/countingsortone/Solution.java
@@ -0,0 +1,32 @@
+package algorithms.countingsortone;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        
+        int digitCount = scanner.nextInt();
+        int[] countResult = new int[100];
+        
+        for (int i = 0; i < digitCount; i++) {
+            countResult[scanner.nextInt()]++;
+        };
+        
+        printArray(countResult);
+    }
+    
+    private static void printArray(int[] array) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+            sb.append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
