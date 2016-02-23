bb5a9c42d4736f813396a53888001862b4ba7241
webdude21 <dimo.petrof@gmail.com>
2016-02-23 23:15:01 +0200

    counting sort 3

---
+++ Algorithms/src/algorithms/countingsortthree/Solution.java
@@ -0,0 +1,36 @@
+package algorithms.countingsortthree;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        int numberCount[] = new int[100];
+        
+        for (int i = 0; i <= n; i++) {
+            String[] words = scanner.nextLine().split(" ");
+            if (!words[0].equals("")) {
+                int number = Integer.parseInt(words[0]);
+                numberCount[number] += 1;
+            }
+        }
+        
+        int sum = 0;
+        
+        for (int i = 0; i < numberCount.length; i++) {
+            sum = 0;
+            for (int j = 0; j <= i; j++)
+                sum += numberCount[j];
+
+            System.out.print(sum + " ");
+        }
+        scanner.close();
+
+    }
+
+}
