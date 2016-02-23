fb35c303e36e3641bea8ba56d0911bebd320b1a2
webdude21 <dimo.petrof@gmail.com>
2016-02-23 23:17:14 +0200

    counting sort three formated

--- Algorithms/src/algorithms/countingsortthree/Solution.java
+++ Algorithms/src/algorithms/countingsortthree/Solution.java
@@ -9,9 +9,9 @@ public class Solution {
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        
+
         int numberCount[] = new int[100];
-        
+
         for (int i = 0; i <= n; i++) {
             String[] words = scanner.nextLine().split(" ");
             if (!words[0].equals("")) {
@@ -19,18 +19,17 @@ public class Solution {
                 numberCount[number] += 1;
             }
         }
-        
+
         int sum = 0;
-        
+
         for (int i = 0; i < numberCount.length; i++) {
             sum = 0;
-            for (int j = 0; j <= i; j++)
+            
+            for (int j = 0; j <= i; j++) {
                 sum += numberCount[j];
+            }
 
             System.out.print(sum + " ");
         }
-        scanner.close();
-
     }
-
 }
