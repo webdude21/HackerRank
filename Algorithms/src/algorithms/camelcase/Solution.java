d3b1b337e4a11651a4f0299ad8f8ddefddb5942c
webdude21 <dimo.petrof@gmail.com>
2016-09-03 16:24:09 +0300

    solved simple cammelCase

---
+++ Algorithms/src/algorithms/camelcase/Solution.java
@@ -0,0 +1,18 @@
+package algorithms.camelcase;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(countWords(scanner.nextLine()));
+	}
+
+	private static long countWords(String input) {
+		return input
+				.chars()
+				.filter(Character::isUpperCase)
+				.count() + 1;
+	}
+}
