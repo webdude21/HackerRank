ac816737dfe5af02bfa2b2ab168910c4e2c8a833
webdude <dimo.petrof@gmail.com>
2017-01-08 19:23:51 +0200

    Array rotation

---
+++ Algorithms/src/algorithms/arrayrotation/Solution.java
@@ -0,0 +1,45 @@
+package algorithms.arrayrotation;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] array = new int[scanner.nextInt()];
+		int rotations = readInput(scanner, array);
+		printArrayWithRotation(array, rotations);
+	}
+
+	private static int readInput(Scanner scanner, int[] array) {
+		int rotations = scanner.nextInt();
+
+		scanner.nextLine();
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = scanner.nextInt();
+		}
+		return rotations;
+	}
+
+	private static void printArrayWithRotation(int[] array, int rotations) {
+		StringBuilder sb = new StringBuilder();
+
+		if (rotations > array.length) {
+			rotations = array.length % rotations;
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			int adjustedIndex = i + rotations;
+
+			if (adjustedIndex >= array.length) {
+				adjustedIndex = adjustedIndex - array.length;
+			}
+
+			sb.append(array[adjustedIndex]);
+			sb.append(" ");
+		}
+
+		System.out.println(sb.toString().trim());
+	}
+}
