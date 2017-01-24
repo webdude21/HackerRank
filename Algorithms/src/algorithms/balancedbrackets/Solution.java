60bffcedfa094b905b341ab5f43b518548be663f
webdude <dimo.petrof@gmail.com>
2017-01-24 08:09:43 +0200

    solved matching brackets

---
+++ Algorithms/src/algorithms/balancedbrackets/Solution.java
@@ -0,0 +1,46 @@
+package algorithms.balancedbrackets;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+
+	private static Map<Character, Character> bracketPairs = new HashMap<>();
+
+	public static void main(String[] args) {
+		bracketPairs.put('{', '}');
+		bracketPairs.put('[', ']');
+		bracketPairs.put('(', ')');
+
+
+		Scanner in = new Scanner(System.in);
+		int lines = in.nextInt();
+
+		for (int i = 0; i < lines; i++) {
+			System.out.println((isBalanced(in.next())) ? "YES" : "NO");
+		}
+	}
+
+	private static boolean isBalanced(String expression) {
+		if (expression.length() % 2 == 1) {
+			return false;
+		}
+
+		char[] brackets = expression.toCharArray();
+		Stack<Character> s = new Stack<>();
+		for (char bracket : brackets) {
+			if (bracketPairs.containsKey(bracket)) {
+				s.push(bracketPairs.get(bracket));
+			} else {
+				if (s.empty() || bracket != s.peek()) {
+					return false;
+				}
+				s.pop();
+			}
+		}
+
+		return s.empty();
+	}
+}
