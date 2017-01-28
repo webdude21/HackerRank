8bdff51ec13f958cc91e6fbf62617e90f4aa4b9b
webdude <dimo.petrof@gmail.com>
2017-01-28 16:07:27 +0200

    solve coin change problem with dp

---
+++ Algorithms/src/algorithms/coinchange/Solution.java
@@ -0,0 +1,31 @@
+package algorithms.coinchange;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	private static long makeChange(int[] coins, int amount) {
+		int[] solutions = new int[amount + 1];
+
+		solutions[0] = 1;
+
+		for (int coin : coins) {
+			for (int j = coin; j <= amount; j++) {
+				solutions[j] += solutions[j - coin];
+			}
+		}
+
+		return solutions[amount];
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int coins[] = new int[m];
+		for (int coins_i = 0; coins_i < m; coins_i++) {
+			coins[coins_i] = in.nextInt();
+		}
+		System.out.println(makeChange(coins, n));
+	}
+}
