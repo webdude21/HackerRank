13dca776ec95157218662d6b8064f177fd13bc37
webdude <dimo.petrof@gmail.com>
2017-01-28 11:04:42 +0200

    adding bubble sort

---
+++ Algorithms/src/algorithms/bubblesort/Solution.java
@@ -0,0 +1,40 @@
+package algorithms.bubblesort;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int arrayToSort[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			arrayToSort[i] = in.nextInt();
+		}
+
+		bubbleSort(arrayToSort, arrayToSort.length);
+	}
+
+	private static void bubbleSort(int[] arrayToSort, int n) {
+		int totalSwaps = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n - 1; j++) {
+				if (arrayToSort[j] > arrayToSort[j + 1]) {
+					swap(arrayToSort, j, j + 1);
+					totalSwaps++;
+				}
+			}
+		}
+
+		System.out.printf("Array is sorted in %d swaps.%n", totalSwaps);
+		System.out.printf("First Element: %d%n", arrayToSort[0]);
+		System.out.printf("Last Element: %d%n", arrayToSort[arrayToSort.length - 1]);
+	}
+
+	private static void swap(int[] array, int a, int b) {
+		int swap = array[a];
+		array[a] = array[b];
+		array[b] = swap;
+	}
+}
