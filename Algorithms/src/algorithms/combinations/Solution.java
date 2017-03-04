f0ee300f091087ae6d75b00ea8d261146a599aac
webdude <dimo.petrof@gmail.com>
2017-03-04 15:50:18 +0200

    added combination generator

---
+++ Algorithms/src/algorithms/combinations/Solution.java
@@ -0,0 +1,42 @@
+package algorithms.combinations;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	private static int combinationCount = 0;
+
+	private static StringBuilder stringBuilder = new StringBuilder();
+
+	public static void main(String[] args) {
+		int n = scanner.nextInt();
+		int[] elements = new int[n];
+		generateCombinations(elements, 0);
+		System.out.println(stringBuilder.toString());
+		System.out.printf("Just printed %d combinations %n", combinationCount);
+	}
+
+	private static void generateCombinations(int[] elements, int index) {
+		if (index >= elements.length) {
+			combinationCount++;
+			addToStringBuilder(elements);
+			return;
+		}
+
+		for (int i = 0; i < elements.length; i++) {
+			elements[index] = i + 1;
+			generateCombinations(elements, index + 1);
+		}
+	}
+
+	private static void addToStringBuilder(int[] elements) {
+		for (int element : elements) {
+			stringBuilder.append(element);
+			stringBuilder.append(',');
+		}
+
+		stringBuilder.append(System.lineSeparator());
+	}
+}
