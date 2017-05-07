22d8ef3f477f97108a1ca9ab5123b4f0339d4230
webdude <dimo.petrof@gmail.com>
2017-05-07 15:29:26 +0300

    solved funny string

---
+++ Algorithms/src/algorithms/funnystring/Solution.java
@@ -0,0 +1,32 @@
+package algorithms.funnystring;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int testCases = scanner.nextInt();
+		scanner.nextLine();
+
+		for (int i = 0; i < testCases; i++) {
+			System.out.println(isFunny(scanner.nextLine()) ? "Funny" : "Not Funny");
+		}
+	}
+
+	private static boolean isFunny(String input) {
+		String reversed = new StringBuilder(input).reverse().toString();
+
+		for (int i = 1; i < reversed.length(); i++) {
+			if (formula(input, i) != formula(reversed, i)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static int formula(String input, int indexStart) {
+		return Math.abs(input.charAt(indexStart - 1) - input.charAt(indexStart));
+	}
+}
