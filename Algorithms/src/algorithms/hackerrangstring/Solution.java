3c1f567c791ca1d7993a07f1dd63bc83e5fdc12a
webdude <dimo.petrof@gmail.com>
2017-05-27 15:26:45 +0300

    added hackerrank String solution

---
+++ Algorithms/src/algorithms/hackerrangstring/Solution.java
@@ -0,0 +1,31 @@
+package algorithms.hackerrangstring;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int testCases = Integer.parseInt(scanner.nextLine());
+
+		IntStream
+			.range(0, testCases)
+			.mapToObj(x -> scanner.nextLine())
+			.map(x -> isWorkContainedInString(x, "hackerrank"))
+			.map(x -> x ? "YES" : "NO")
+			.forEach(System.out::println);
+	}
+
+	private static boolean isWorkContainedInString(String input, String wordToLookFor) {
+		int lookupWordIndex = 0;
+
+		for (int i = 0; i < input.length() && lookupWordIndex < wordToLookFor.length(); i++) {
+			if (input.charAt(i) == wordToLookFor.charAt(lookupWordIndex)) {
+				lookupWordIndex++;
+			}
+		}
+
+		return lookupWordIndex == wordToLookFor.length();
+	}
+}
