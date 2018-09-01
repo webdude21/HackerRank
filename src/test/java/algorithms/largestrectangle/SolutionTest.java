d1084804c36bb4bcde672901112a223791a216b1
webdude21 <dimo.petrof@gmail.com>
2018-09-01 16:04:57 +0300

    added a couple of more tests

--- src/test/java/algorithms/largestrectangle/SolutionTest.java
+++ src/test/java/algorithms/largestrectangle/SolutionTest.java
@@ -13,7 +13,9 @@ public class SolutionTest implements ArgumentsProvider {
   @Override
   public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
     return Stream.of(
-      Arguments.of(9, new int[]{1, 2, 3, 4, 5})
+      Arguments.of(9, new int[]{1, 2, 3, 4, 5}),
+      Arguments.of(26152, new int[]{8979, 4570, 6436, 5083, 7780, 3269, 5400, 7579, 2324, 2116}),
+      Arguments.of(18060, new int[]{6320, 6020, 6098, 1332, 7263, 672, 9472, 2838, 3401, 9494})
     );
   }
 
