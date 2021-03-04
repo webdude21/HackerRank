af3b4f3421720c5eec1a4c4d7c64d2ddc3792300
webdude21 <dimo.petrof@gmail.com>
2021-03-04 10:33:01 +0200

    solve array rotation with Java's API

--- src/main/java/algorithms/arrayrotation/SolutionWithJavaApi.java
+++ src/main/java/algorithms/arrayrotation/SolutionWithJavaApi.java
@@ -10,10 +10,16 @@ public class SolutionWithJavaApi {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+
     int listSize = scanner.nextInt();
     int rotations = scanner.nextInt();
+
     scanner.nextLine();
-    List<String> integers = IntStream.range(0, listSize).mapToObj(i -> String.valueOf(scanner.nextInt())).collect(Collectors.toList());
+
+    List<String> integers = IntStream.range(0, listSize)
+      .mapToObj(i -> String.valueOf(scanner.nextInt()))
+      .collect(Collectors.toList());
+
     Collections.rotate(integers, listSize - rotations);
     System.out.println(String.join(" ", integers));
   }
